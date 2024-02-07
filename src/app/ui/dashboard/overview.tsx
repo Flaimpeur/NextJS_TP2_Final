971123495e4d78c02ed9cfef2c4071a185321310
Flaimpeur <114905674+Flaimpeur@users.noreply.github.com>
2024-02-07 17:23:20 +0100

    avancement 17h23 création bouton pour formulaire fait

--- src/app/ui/dashboard/overview.tsx
+++ src/app/ui/dashboard/overview.tsx
@@ -1,4 +1,6 @@
 import { lusitana } from '@/app/ui/fonts';
+import {ArrowDownTrayIcon, ArrowUpTrayIcon} from '@heroicons/react/24/outline';
+import Link from 'next/link';
 
 export default function Overview(){
     return(
@@ -6,6 +8,16 @@ export default function Overview(){
             <div className="flex h-full flex-col px-3 py-4 md:px-2">
                 <div className="flex items-center">
                     <p className={`${lusitana.className} text-5xl font-bold`}>15,000.33€</p>
+                </div>
+                <div className='grid gap-6 sm:grid-cols-2 w-max'>
+                    <Link href={`/dashboard/amodifier`} className="rounded-md border p-2 bg-slate-50 hover:bg-zinc-400 flex items-center">
+                        <ArrowUpTrayIcon className="w-5" />
+                        <p>Send</p>
+                    </Link>
+                    <Link href={`/dashboard/amodifier`} className="rounded-md border p-2 bg-slate-50 hover:bg-zinc-400 flex items-center">
+                        <ArrowDownTrayIcon className="w-5" />
+                        <p>Receive</p>
+                    </Link>
                 </div>
             </div>
         </div>
